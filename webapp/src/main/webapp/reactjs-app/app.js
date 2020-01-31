63b0e38c8273098e42c09d4a3ffa583397c68378
Danylov <ol_danylov@ukr.net>
2020-01-31 10:55:35 +0200

    Implementation of task items 1, 2 (partially), 3, 4, 6 (partially), 7.

--- webapp/src/main/webapp/reactjs-app/app.js
+++ webapp/src/main/webapp/reactjs-app/app.js
@@ -5,34 +5,82 @@ export default class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {poster: "system"};
+        const currDate = new Date().toISOString().slice(0, 10);
+        this.state = {
+            w_tt_inp: '47%',
+            w_oth_inp: '12%',
+            w_oth_mrg: '0.3%',
+            vis_oth: "visible",
+            vis_f1: "visible",
+            vis_f2: "hidden",
+            currDate : currDate
+        };
+        this.oth_inpChange = (e) => {
+            (e.target.value === "system") ?
+                this.setState({
+                    w_tt_inp: '73%',
+                    w_oth_inp: '0%',
+                    w_oth_mrg: '0%',
+                    vis_oth: "hidden"
+                }) :
+                this.setState({
+                    w_tt_inp: '47%',
+                    w_oth_inp: '12%',
+                    w_oth_mrg: '0.3%',
+                    vis_oth: "visible"
+                });
+        }
+        this.b_plusClick = (e) => {
+            this.setState({vis_f2: "visible"});
+        }
+        this.b_f1xClick = (e) => {
+            this.setState({vis_f1: "hidden", vis_f2: "hidden", vis_oth : "hidden"});
+        }
+        this.b_f2xClick = (e) => {
+            this.setState({vis_f2: "hidden"});
+        }
     }
 
-    render() {
+    render()
+    {
         return (
             <section>
-                <form id="f1" className="form-horizontal">
+                <form id="f1" className="form-horizontal" style={{visibility: this.state.vis_f1}}>
                     <div className="form-group">
                         <div className="main_cont1  d-flex">
                             <button type="button" className="btn btn-default">
                                 <i className="uiIconQuestion uiIconDarkGray"/>
                             </button>
-                            <input className="tt_inp"   placeholder="Tip text ..."/>
-                            <input className="oth_inp"  placeholder="via $USER"/>
-                            <input className="oth_inp"  placeholder="$DATETIME"/>
+                            <input className="tt_inp" placeholder="Tip text ..."
+                                   style={{width: this.state.w_tt_inp}}/>
+                            <input className="oth_inp" placeholder="via $USER"
+                                   style={{
+                                       width: this.state.w_oth_inp,
+                                       margin: this.state.w_oth_mrg,
+                                       visibility: this.state.vis_oth
+                                   }}
+                                   onChange={(e) => this.oth_inpChange(e)}/>
+                            <input  className="oth_inp"  placeholder="$DATETIME"  defaultValue={this.state.currDate}
+                                    style={{
+                                        width: this.state.w_oth_inp,
+                                        margin: this.state.w_oth_mrg,
+                                        visibility: this.state.vis_oth
+                                    }}/>
                             <button type="button" className="btn btn-default">
                                 <span className="uiIconNew uiIconDarkGray"/>
                             </button>
-                            <button type="button" className="btn btn-default">
+                            <button type="button" className="btn btn-default"
+                                    onClick={(e) => this.b_plusClick(e)}>
                                 <span className="uiIconSimplePlusMini uiIconDarkGray"/>
                             </button>
-                            <button type="button" className="btn btn-default">
+                            <button type="button" className="btn btn-default"
+                                    onClick={(e) => this.b_f1xClick(e)}>
                                 <span className="uiIconClose uiIconDarkGray"/>
                             </button>
                         </div>
                     </div>
                 </form>
-                <form id="f2" className="form-horizontal">
+                <form id="f2" className="form-horizontal" style={{visibility: this.state.vis_f2}}>
                     <div className="form-group">
                         <div className="main_cont2  d-flex">
                             <button type="button" className="btn btn-default">
@@ -40,7 +88,8 @@ export default class App extends Component {
                             </button>
                             <textarea className="t_a" rows="3" placeholder="Tip text..."/>
                             <button type="submit" id="s_b" className="btn btn-default">Save</button>
-                            <button type="button" className="btn btn-default">
+                            <button type="button" className="btn btn-default"
+                                    onClick={(e) => this.b_f2xClick(e)}>
                                 <span className="uiIconClose uiIconDarkGray"/>
                             </button>
                         </div>
